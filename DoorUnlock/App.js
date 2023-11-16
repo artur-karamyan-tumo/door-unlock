f6e26e1696feb8a9d7806f1b7bccd4bb3ae35188
Artur Karamyan <artur.karamyan@tumo.org>
2023-11-16 12:20:32 +0400

    open door button

--- DoorUnlock/App.js
+++ DoorUnlock/App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your Open sesame app!</Text>
+        <Button
+            onPress={() => {
+                fetch('http://109.75.44.3:9000/gateopen/ProductEngineering.php')
+                    .then(response => response.json())
+                    .then(json => {
+                        console.log(json);
+                    })
+                    .catch(error => {
+                        console.error(error);
+                    });
+            }}
+            title="Open Door"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+        />
       <StatusBar style="auto" />
     </View>
   );
